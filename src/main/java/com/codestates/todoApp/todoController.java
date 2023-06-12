568658ee4d82b9821a17fe37942131449334003d
HANBYEOLCHOI <soalinerstar@gmail.com>
2023-06-12 20:14:44 +0900

    홈 화면 구성 완료

---
+++ src/main/java/com/codestates/todoApp/todoController.java
@@ -0,0 +1,14 @@
+package com.codestates.todoApp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class todoController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> todoHome() {
+        return ResponseEntity.ok("To-do Application !");
+    }
+}
